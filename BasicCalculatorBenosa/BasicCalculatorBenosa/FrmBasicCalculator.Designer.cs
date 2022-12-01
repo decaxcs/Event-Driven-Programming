794449ba8985026774359d5b681a34d2b3d3466c
Fhranz Joseph Abella Benosa <97322613+decaxcs@users.noreply.github.com>
2022-12-01 17:31:07 +0800

    Add files via upload

---
+++ BasicCalculatorBenosa/BasicCalculatorBenosa/FrmBasicCalculator.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace BasicCalculatorBenosa
+{
+    partial class FrmBasicCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNum1 = new System.Windows.Forms.TextBox();
+            this.cbOperations = new System.Windows.Forms.ComboBox();
+            this.tbNum2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Basic Calculator";
+            // 
+            // tbNum1
+            // 
+            this.tbNum1.Location = new System.Drawing.Point(16, 36);
+            this.tbNum1.Name = "tbNum1";
+            this.tbNum1.Size = new System.Drawing.Size(256, 20);
+            this.tbNum1.TabIndex = 1;
+            this.tbNum1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // cbOperations
+            // 
+            this.cbOperations.FormattingEnabled = true;
+            this.cbOperations.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cbOperations.Location = new System.Drawing.Point(16, 62);
+            this.cbOperations.Name = "cbOperations";
+            this.cbOperations.Size = new System.Drawing.Size(256, 21);
+            this.cbOperations.TabIndex = 2;
+            this.cbOperations.SelectedIndexChanged += new System.EventHandler(this.cbOperations_SelectedIndexChanged);
+            // 
+            // tbNum2
+            // 
+            this.tbNum2.Location = new System.Drawing.Point(16, 89);
+            this.tbNum2.Name = "tbNum2";
+            this.tbNum2.Size = new System.Drawing.Size(256, 20);
+            this.tbNum2.TabIndex = 3;
+            this.tbNum2.TextChanged += new System.EventHandler(this.tbNum2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Total:\r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Location = new System.Drawing.Point(16, 226);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(256, 23);
+            this.btnCompute.TabIndex = 6;
+            this.btnCompute.Text = "Compute";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblTotal.Location = new System.Drawing.Point(12, 136);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(60, 24);
+            this.lblTotal.TabIndex = 7;
+            this.lblTotal.Text = "00000";
+            this.lblTotal.Click += new System.EventHandler(this.lblTotal_Click);
+            // 
+            // FrmBasicCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbNum2);
+            this.Controls.Add(this.cbOperations);
+            this.Controls.Add(this.tbNum1);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmBasicCalculator";
+            this.Text = "FrmBasicCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNum1;
+        private System.Windows.Forms.ComboBox cbOperations;
+        private System.Windows.Forms.TextBox tbNum2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ BasicCalculatorBenosa/BasicCalculatorBenosa/FrmBasicCalculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BasicComputationBenosa;
+
+namespace BasicCalculatorBenosa
+{
+    public partial class FrmBasicCalculator : Form
+    {
+        float total;
+        float num1, num2;
+        public FrmBasicCalculator()
+        {
+            InitializeComponent();
+            num1 = 0;
+            num2 = 0;
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            num1 = Int64.Parse(tbNum1.Text);
+            num2 = Int64.Parse(tbNum2.Text);
+
+            if (cbOperations.SelectedIndex == 0)
+            {
+                total = BasicComputationBenosa.BasicComputationBenosa.Addition(num1, num2);
+            }
+            else if(cbOperations.SelectedIndex == 1)
+            {
+                total = BasicComputationBenosa.BasicComputationBenosa.Subtraction(num1, num2);
+            }
+            else if (cbOperations.SelectedIndex == 2)
+            {
+                total = BasicComputationBenosa.BasicComputationBenosa.Multiplication(num1, num2);
+            }
+            else if (cbOperations.SelectedIndex == 3)
+            {
+                total = BasicComputationBenosa.BasicComputationBenosa.Division(num1, num2);
+            }
+            lblTotal.Text = total.ToString();
+        }
+
+        private void lblTotal_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNum2_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void cbOperations_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
